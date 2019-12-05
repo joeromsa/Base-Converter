411d4993142934a651f8b2a323d92e70dc09cc07
joeromsa <jromsa6@gmail.com>
2019-12-05 15:51:36 -0600

    Added dec to octal and hex

--- converter.c
+++ converter.c
@@ -74,6 +74,16 @@ int decToBin() {
 
 }
 
+int numberCollect()
+{
+	int dec;
+
+	printf("\nEnter a number in decimal to convert\n");
+	scanf("%d", &dec);
+
+	return dec;
+}
+
 
 int main()
 {
@@ -87,7 +97,9 @@ int main()
 		printf("2) Decimal to Binary\n");
 		printf("3) Octal to Decimal\n");
 		printf("4) Decimal to Octal\n");
-		printf("5) Exit\n");
+		printf("5) Hex to Decimal\n");
+		printf("6) Decimal to Hex\n");
+		printf("7) Exit\n");
 
 		scanf("%d", &choice);
 
@@ -95,8 +107,10 @@ int main()
 			case 1: printf("\nThe number in decimal is %d\n\n", binToDec()); break;
 			case 2: printf("\nThe number in binary is %d\n\n", decToBin()); break;
 			case 3: break;
-			case 4: break;
-			case 5: return EXIT_SUCCESS;
+			case 4: printf("\nThe number in octal is %o\n\n", numberCollect()); break;
+			case 5: break;
+			case 6: printf("\nThe number in hex is %X\n\n", numberCollect()); break;
+			case 7: return EXIT_SUCCESS;
 			default: printf("\nInvalid choice\n\n");
 		}
 	}
